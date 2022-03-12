f0c976a2b943f225338760b99f39541f92c44efb
KapinosKrzysztof <krzysztofkapinos2001@gmail.com>
2022-03-12 19:22:47 +0100

    pierwsze dwa zadania

---
+++ main.py
@@ -0,0 +1,30 @@
+#zad 1
+'''
+a = [1-x for x in range(1,11)]
+b = [4**i for i in range(1,8)]
+c = [x for x in b if x % 2 == 0]
+
+print(a)
+print(b)
+print(c)
+'''
+
+#zad 2
+'''
+from random import randint
+
+lista = []
+
+for i in range(0, 10):
+    lista.append(randint(0, 100))
+    lista = [i for i in lista if i % 2 == 0]
+print(lista)
+'''
+
+moj_slownik = {'chleb': 'szt', 'sok': 'litry','bółki': 'szt', 'jabłka': 'kilogramy'}
+
+
+
+
+
+
